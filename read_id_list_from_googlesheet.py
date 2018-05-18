9bad05f1351f7131309802be80bfbb0b04a77e87
Yue <wy790054473@gmail.com>
2018-05-19 00:03:38 +0200

    Add files via upload

---
+++ read_id_list_from_googlesheet.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from StringIO import StringIO
+
+def read_sheet(service):
+    id = '1tZIPyPPpwwmdSndaYv1AyFLB2YoFWJZiITWqjC_mUYo'
+    rangeName = '2017102'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=id, range=rangeName).execute()
+
+    # retrieve data by rows
+    values = result.get('values', [])
+
+    #retrieve profile id list which should be used
+    headers = values[0]
+    index_shouldbeused=headers.index('shouldBeUsed')
+    index_profileid=headers.index('profileId')
+    profile_id_list=[]
+    for i in range(len(values)):
+        if values[i][index_shouldbeused] == 'TRUE':
+            profile_id_list.append(values[i][index_profileid])
+    return profile_id_list
